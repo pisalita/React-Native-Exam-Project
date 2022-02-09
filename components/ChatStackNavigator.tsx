116e1a06bb0156d1f61574fa09b327dbf5134d7d
Jimmi Pisalita <mig.jimmi@gmail.com>
2022-02-09 19:52:18 +0100

    Adding type to stacknavigator

--- components/ChatStackNavigator.tsx
+++ components/ChatStackNavigator.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import Screen1 from "../screens/Screen1";
 import Screen2 from "../screens/Screen2";
 import Screen3 from "../screens/Screen3";
+import { StackParamList } from "../types/navigations";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<StackParamList>();
 
 const ChatStackNavigator = () => {
   return (
--- screens/Screen1.tsx
+++ screens/Screen1.tsx
@@ -1,9 +1,16 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamList } from "../types/navigations";
+
+type ScreenNavigationType = NativeStackNavigationProp<
+  StackParamList,
+  "Screen1"
+>;
 
 const Screen1 = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<ScreenNavigationType>();
 
   return (
     <View>
