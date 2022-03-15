869a9a3f97edd44787e52fb957472320041fc507
Jimmi Pisalita <mig.jimmi@gmail.com>
2022-03-15 10:51:11 +0100

    Cleaner code, combined handlers on create user screen

--- screens/CreateUser.tsx
+++ screens/CreateUser.tsx
@@ -12,47 +12,12 @@ import { useAppDispatch } from "../app/hooks";
 import { createUser } from "../features/User";
 
 const CreateUser = () => {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [displayName, setDisplayName] = useState("");
-  const [photoUrl, setPhotoUrl] = useState("");
   const dispatch = useAppDispatch();
 
-  const onNameChange = (
-    e: NativeSyntheticEvent<TextInputChangeEventData>
-  ): void => {
-    const value = e.nativeEvent.text;
-    setName(value);
-  };
-  const onEmailChange = (
-    e: NativeSyntheticEvent<TextInputChangeEventData>
-  ): void => {
-    const value = e.nativeEvent.text;
-    setEmail(value);
-  };
-  const onPasswordChange = (
-    e: NativeSyntheticEvent<TextInputChangeEventData>
-  ): void => {
-    const value = e.nativeEvent.text;
-    setPassword(value);
-  };
-  const onDisplayNameChange = (
-    e: NativeSyntheticEvent<TextInputChangeEventData>
-  ): void => {
-    const value = e.nativeEvent.text;
-    setDisplayName(value);
-  };
-  const onPhotoUrlChange = (
-    e: NativeSyntheticEvent<TextInputChangeEventData>
-  ): void => {
-    const value = e.nativeEvent.text;
-    setPhotoUrl(value);
-  };
-
-  //how do i call this function and pass e?? ask christian!
   const combinedOnChange = (
-    setter: any,
+    setter: React.Dispatch<React.SetStateAction<string>>,
     e: NativeSyntheticEvent<TextInputChangeEventData>
   ): void => {
     const value = e.nativeEvent.text;
@@ -62,30 +27,25 @@ const CreateUser = () => {
   return (
     <SafeAreaView>
       <Text>Login</Text>
-      <TextInput value={name} onChange={onNameChange} placeholder="name" />
-      <TextInput value={email} onChange={onEmailChange} placeholder="email" />
+      <TextInput
+        value={email}
+        onChange={(e) => {
+          combinedOnChange(setEmail, e);
+        }}
+        placeholder="email"
+      />
       <TextInput
         value={password}
-        onChange={onPasswordChange}
+        onChange={(e) => {
+          combinedOnChange(setPassword, e);
+        }}
         placeholder="password"
-      />
-      <TextInput
-        value={displayName}
-        onChange={onDisplayNameChange}
-        placeholder="display name"
-      />
-      <TextInput
-        value={photoUrl}
-        onChange={onPhotoUrlChange}
-        placeholder="photo url"
       />
       <Button
         title="Create user"
         onPress={() => {
-          if (name && email && displayName && password !== null) {
-            dispatch(
-              createUser({ name, email, password, displayName, photoUrl })
-            );
+          if (email && password !== "") {
+            dispatch(createUser({ email, password }));
           }
         }}
       />
