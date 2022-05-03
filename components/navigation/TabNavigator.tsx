2fdb2a41999f7e6a67ef42c0c7eafe6d10a4d486
Jimmi Pisalita <mig.jimmi@gmail.com>
2022-05-03 19:46:15 +0200

    Added chat icon and implemented active and inactive color

--- components/navigation/TabNavigator.tsx
+++ components/navigation/TabNavigator.tsx
@@ -4,6 +4,7 @@ import { useAppSelector } from "../../app/hooks";
 import ChatStackNavigator from "./ChatStackNavigator";
 import * as SecureStore from "expo-secure-store";
 import MenuStackNavigator from "./MenuStackNavigator";
+import { Image } from "react-native";
 
 const TabNavigator = () => {
   const Tab = createBottomTabNavigator();
@@ -24,7 +25,22 @@ const TabNavigator = () => {
     >
       <Tab.Screen name="Home" component={ChatStackNavigator} />
       <Tab.Screen name="Discover" component={ChatStackNavigator} />
-      <Tab.Screen name="Chat" component={ChatStackNavigator} />
+      <Tab.Screen
+        name="Chat"
+        component={ChatStackNavigator}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require("../../chatIcon.png")}
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: focused ? "#5050A5" : "#707070",
+              }}
+            />
+          ),
+        }}
+      />
       <Tab.Screen name="Menu" component={MenuStackNavigator} />
     </Tab.Navigator>
   );
